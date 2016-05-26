b51992ea9ae0e134ed63209cb80a4659eb0019e2
Boudhayan Gupta <me@BaloneyGeek.com>
2016-05-26 23:27:51 +0530

    Update copyright notices

--- server/Server.py
+++ server/Server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # This file is part of Propagator, a KDE Sysadmin Project
 #
-# Copyright 2015 Boudhayan Gupta <bgupta@kde.org>
+#   Copyright 2015-2016 (C) Boudhayan Gupta <bgupta@kde.org>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
